37c658c79ea6432bf1fb6d4a53f2fdd4fc2489db
Duong Dat <infrmtcs@191-233.priv23.nus.edu.sg>
2015-04-03 02:24:11 +0800

    Update pictures

--- CattleBattle/CattleBattle/GameViewController.swift
+++ CattleBattle/CattleBattle/GameViewController.swift
@@ -123,14 +123,6 @@ class GameViewController: UIViewController {
     override func shouldAutorotate() -> Bool {
         return true
     }
-//
-//    override func supportedInterfaceOrientations() -> Int {
-//        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-//            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
-//        } else {
-//            return Int(UIInterfaceOrientationMask.All.rawValue)
-//        }
-//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
