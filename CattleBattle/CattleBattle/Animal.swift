8365df0c1ca549b270726e6fe9376e7bba24636f
Duong Dat <infrmtcs@191-233.priv23.nus.edu.sg>
2015-04-03 01:14:29 +0800

    modify Animal

--- CattleBattle/CattleBattle/Animal.swift
+++ CattleBattle/CattleBattle/Animal.swift
@@ -3,12 +3,19 @@
 //  CattleBattle
 //
 //  Created by Ding Ming on 27/3/15.
-//  Copyright (c) 2015 Ding Ming. All rights reserved.
+//  Copyright (c) 2015 Ding Ming. All rights deployed.
 //
 
-import UIKit
+import SpriteKit
 
 class Animal {
+    
+    enum Color: String {
+        case WHITE = "white"
+        case BLACK = "black"
+        
+        internal static let allColors = [WHITE, BLACK]
+    }
     
     enum Size: String {
         case TINY = "1"
@@ -16,23 +23,61 @@ class Animal {
         case MEDIUM = "3"
         case LARGE = "4"
         case HUGE = "5"
+        
+        internal static let allSizes = [TINY, SMALL, MEDIUM, LARGE, HUGE]
     }
     
-    enum Color: String {
-        case WHITE = "white"
-        case BLACK = "black"
+    enum Status: String {
+        case DEPLOYED = "goat"
+        case ENGAGED = "engaged"
+        case BUTTON = "button"
+
+        internal static let allStatuses = [DEPLOYED, ENGAGED, BUTTON]
     }
     
-    var color: Color
-    var size: Size
+    struct Constants {
+        internal static var goatTextures = Color.allColors.map() { (color) -> [SKTexture] in
+            return Size.allSizes.map() { (size) -> SKTexture in
+                return SKTexture(imageNamed: Animal.getImageName(color, size))
+            }
+        }
+        internal static var buttonTextures = Color.allColors.map() { (color) -> [SKTexture] in
+            return Size.allSizes.map() { (size) -> SKTexture in
+                return SKTexture(imageNamed: Animal.getImageName(color, size))
+            }
+        }
+    }
     
-    func getImageName() -> String {
+    internal var color: Color
+    internal var size: Size
+    internal var status: Status
+    
+    func getImageFileName() -> String {
+        return status.rawValue + "-" + color.rawValue + "-" + size.rawValue + ".png"
+    }
+    
+    func getTexture() -> SKTexture {
+        if status == .BUTTON {
+            return Constants.buttonTextures[find(Color.allColors, color)!][find(Size.allSizes, size)!]
+        } else {
+            return Constants.goatTextures[find(Color.allColors, color)!][find(Size.allSizes, size)!]
+        }
+    }
+    
+    class func getImageName(color: Color, _ size: Size) -> String {
         return "goat-" + color.rawValue + "-" + size.rawValue + ".png"
     }
+    
+    /*To be removed----------------------------------------------------*/
+    func getImageName() -> String {
+        return Animal.getImageName(color, size)
+    }
+    /*-----------------------------------------------------------------*/
     
     init(color: Color, size: Size) {
         self.color = color
         self.size = size
+        self.status = .DEPLOYED
     }
     
     let scale1x : CGFloat = 0.030
@@ -57,3 +102,4 @@ class Animal {
     }
     
 }
+
---
+++ CattleBattle/CattleBattle/AnimalNode.swift
@@ -0,0 +1,12 @@
+//
+//  GoatNode.swift
+//  CattleBattle
+//
+//  Created by Duong Dat on 4/2/15.
+//  Copyright (c) 2015 Cattle Battle. All rights reserved.
+//
+
+import SpriteKit
+
+class AnimalNode: SKSpriteNode {
+}
