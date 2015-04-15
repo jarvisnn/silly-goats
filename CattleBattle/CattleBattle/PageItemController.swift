5b65c476a7ae9e6634c40cbd366216151eee603f
Thien Tran <thientran1707@gmail.com>
2015-04-15 20:53:20 +0800

    add instruction

---
+++ CattleBattle/CattleBattle/PageItemController.swift
@@ -0,0 +1,15 @@
+//
+//  ItemContentController.swift
+//  CattleBattle
+//
+//  Created by Tran Cong Thien on 14/4/15.
+//  Copyright (c) 2015 Cattle Battle. All rights reserved.
+//
+import UIKit
+
+class PageItemController: UIViewController {
+    // MARK: - Variables
+    var itemIndex: Int = 0
+   
+}
+
